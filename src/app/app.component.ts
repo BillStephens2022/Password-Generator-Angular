21d58ed732e43eb9b6b010eb7d87c71222f69cdd
BillStephens2022 <two4onebill@yahoo.com>
2023-09-01 08:35:42 -0400

    binded function to button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Password-Generator-Angular';
+  onButtonClick() {
+    console.log('Button was clicked');
+  }
 }
